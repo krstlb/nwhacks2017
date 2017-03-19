c6f19e215f7098088750cf7533270271330791ac
Spenser Lee <Spenser.L159@gmail.com>
2017-03-19 00:13:20 -0700

    reading files into gui

--- nwHacks2017/nwHacks2017/MainWindow.xaml.cs
+++ nwHacks2017/nwHacks2017/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,54 @@ namespace nwHacks2017
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private async void openFileBtn_Click(object sender, RoutedEventArgs e)
+        {
+            // Create OpenFileDialog 
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+            // Set filter for file extension and default file extension 
+            //dlg.DefaultExt = ".png";
+            //dlg.Filter = "JPEG Files (*.jpeg)|*.jpeg|PNG Files (*.png)|*.png|JPG Files (*.jpg)|*.jpg|GIF Files (*.gif)|*.gif";
+
+            // Display OpenFileDialog by calling ShowDialog method 
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // Get the selected file name and display in a TextBox 
+            if (result.HasValue && result.Value)
+            {
+                // Open document 
+                string filename = dlg.FileName;
+
+                try
+                {
+                    using (StreamReader sr = new StreamReader(filename))
+                    {
+                        String line = await sr.ReadToEndAsync();
+                        textField.Text = line;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.Write("Could not read the file");
+                }
+            }
+        }
+
+        private void finishBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void resetBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void startBtn_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
